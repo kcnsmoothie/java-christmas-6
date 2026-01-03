90b6ea8439f2fcf7844f2d9a14801ddedef06f99
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-01-03 15:35:40 +0900

    refactor: 메뉴 이름과 가격을 구하는 메서드 분리

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -24,38 +24,12 @@ public class Controller {
 
     public void run() {
         int date = getDate();
-        while (true) {
-            try {
-                String inputMenuAmount = inputView.inputMenuAmount();
-                List<String> parsedMenuAmount = Parser.parseByDelimeter(inputMenuAmount);
-
-                List<String> parsedList = new ArrayList<>();
-                for (String str : parsedMenuAmount) {
-                    Arrays.stream(str.split("-"))
-                            .map(String::trim)
-                            .forEach(parsedList::add);
-                }
-
-                List<String> menuName = new ArrayList<>();
-                for (int i = 0; i < parsedList.size(); i += 2) {
-                    menuName.add(parsedList.get(i));
-                }
-
-                List<String> strAmount = new ArrayList<>();
-                for (int i = 1; i < parsedList.size(); i += 2) {
-                    strAmount.add(parsedList.get(i));
-                }
-                List<Integer> amount = Parser.stringToInt(strAmount);
-
-                Map<Menu, Integer> order = menuService.calculateOrder(menuName, amount);
-
-                int beforeDcTotal = menuService.calculateBeforeDcTotal(order);
-                System.out.println(beforeDcTotal);
-
-            } catch (IllegalArgumentException e) {
-                outputView.printErrorMessage(e);
-            }
-        }
+        List<String> parsedMenuAmountList = getMenuAmount();
+        List<String> menuName = getMenuName(parsedMenuAmountList);
+        List<Integer> amount = getAmount(parsedMenuAmountList);
+        Map<Menu, Integer> order = menuService.calculateOrder(menuName, amount);
+        int beforeDcTotal = menuService.calculateBeforeDcTotal(order);
+        System.out.println(beforeDcTotal);
     }
 
     private int getDate() {
@@ -65,6 +39,57 @@ public class Controller {
                 int intDate = Parser.stringToInt(inputDate);
                 Validator.validateNumberRange(intDate);
                 return intDate;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private List<String> getMenuAmount() {
+        while (true) {
+            try {
+                String inputMenuAmount = inputView.inputMenuAmount();
+                List<String> parsedMenuAmount = Parser.parseByDelimeter(inputMenuAmount);
+                List<String> parsedMenuAmountList = new ArrayList<>();
+                for (String str : parsedMenuAmount) {
+                    Arrays.stream(str.split("-"))
+                            .map(String::trim)
+                            .forEach(parsedMenuAmountList::add);
+                }
+                return parsedMenuAmountList;
+
+
+
+
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private List<String> getMenuName(List<String> parsedMenuAmountList) {
+        while (true) {
+            try {
+                List<String> menuName = new ArrayList<>();
+                for (int i = 0; i < parsedMenuAmountList.size(); i += 2) {
+                    menuName.add(parsedMenuAmountList.get(i));
+                }
+                return menuName;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private List<Integer> getAmount(List<String> parsedMenuAmountList) {
+        while (true) {
+            try {
+                List<String> strAmount = new ArrayList<>();
+                for (int i = 1; i < parsedMenuAmountList.size(); i += 2) {
+                    strAmount.add(parsedMenuAmountList.get(i));
+                }
+                List<Integer> amount = Parser.stringToInt(strAmount);
+                return amount;
             } catch (IllegalArgumentException e) {
                 outputView.printErrorMessage(e);
             }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,31 @@
 package christmas.view;
 
 public class OutputView {
-    public void printPurchaseResult(int purchaseAmount) {
+    public void printPurchaseResult() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printBeforeDcTotal() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printGiftMenu() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printBenefit() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printBenefitAmount() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printAfterDcTotal() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printDecemberEventBadge() {
         System.out.println("<주문 메뉴>");
     }
 
